838ca3dfb8f2be2d7d31b213ef50923a762a034d
wnuken <wnuken@gmail.com>
2016-08-05 14:16:54 -0500

    Vivienda servicios

--- application/modules/modgasmenfrehogar/views/setionsDEF/formC.php
+++ application/modules/modgasmenfrehogar/views/setionsDEF/formC.php
@@ -17,8 +17,8 @@
 			</div>
 			<div class="col-sm-8">
 				<h3><font color="#ec971f"><?php echo $TITULO1; ?></font></h3>
-					<h4><?php echo $TITULO2; ?></h4>
-					<h4><?php echo $TITULO3; ?></h4>
+				<h4><?php echo $TITULO2; ?></h4>
+				<h4><?php echo $TITULO3; ?></h4>
 			</div>
 
 
@@ -35,7 +35,7 @@
 								<div class="col-sm-12">
 									<label class="control-label">¿Su vivienda cuenta con el servicio de alumbrado público?</label>
 								</div>
-								<div class="col-sm-12">
+								<div class="col-sm-12" id="page0">
 									<div>
 										<input type="radio" name="inititalvalue" value="1" id="inititalvalue1" ng-model="Formulario.valorVariable" required>
 										<span></span><label>Si</label>
@@ -57,19 +57,18 @@
 				<div class="row text-center" ng-if="FormServicios.$valid">
 					<button class="btn btn-success"  ng-click="validateForm1(2)" id="ENV_2_2">Guardar y Continuar <span class="glyphicon glyphicon-chevron-right" aria-hidden="true" title="Continuar"></span></button>
 				</div>
-					<div class="row text-center" ng-if="!FormServicios.$valid">
-						<button class="btn btn-success" ng-click="validateContinue(0)" id="ENV_2_2">Guardar y Continuar
-							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true" title="Continuar"></span>
-						</button>
-					</div>
-
+				<div class="row text-center" ng-if="!FormServicios.$valid">
+					<button class="btn btn-success" ng-click="validateContinue(0)" id="ENV_2_2">Guardar y Continuar
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true" title="Continuar"></span>
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
 	<div ng-if="pagesection == 2">
 		<div class="row">
 			<div class="col-sm-12">
-				<form class="form-enph" id="FormServicios" name="FormServicios" class="FormServicios">
+				<form class="form-enph" id="FormServicios1" name="FormServicios1" class="FormServicios1">
 					<div class="row">
 						<div class="col-sm-12">
 							<h4></h4>
@@ -78,7 +77,7 @@
 									<tr class="active">
 										<th>Servicio Público Domiciliario</th>
 										<th><div class="text-center">
-										¿Cuánto pagó este hogar la última vez por cada uno de los siguientes servicios públicos domiciliarios?
+											¿Cuánto pagó este hogar la última vez por cada uno de los siguientes servicios públicos domiciliarios?
 										</div></th>
 										<th>¿A cuántos meses correspondió ese pago?</th>
 										<th>Para dar estos datos, ¿verificó el valor con la factura del servicio?</th>
@@ -87,52 +86,52 @@
 									<tr ng-if="Formulario.valorVariable == 1" ng-repeat="servicio in alumbrado">
 										<th>{{servicio.servicio}}</th>
 										<td>
-										<div style="max-width: 900px;">
-											<div class="col-sm-12">
-												<div class="form-group" ng-if="!Formulario.valor[servicio.id]">
-												<div class="input-group">
-													<span class="input-group-addon">Valor</span>
-													<input type="text" name="valor{{servicio.id}}" id="valor{{servicio.id}}" required 
-													ng-model="Formulario.servicios[servicio.idValor]" class="form-control">
+											<div style="max-width: 900px;">
+												<div class="col-sm-12">
+													<div class="form-group" ng-if="!Formulario.valor[servicio.id]">
+														<div class="input-group">
+															<span class="input-group-addon">Valor</span>
+															<input type="text" name="valor{{servicio.id}}" id="valor{{servicio.id}}" required 
+															ng-model="Formulario.servicios[servicio.idValor]" class="form-control">
+														</div>
+													</div>
+													<div class="form-group" ng-if="Formulario.valor[servicio.id]">
+														<div class="input-group">
+															<span class="input-group-addon">Valor</span>
+															<input type="text" name="valor{{servicio.id}}" id="valor{{servicio.id}}" readonly class="form-control" 
+															ng-click="activeValor(servicio.id, servicio.idValor)">
+														</div>
 													</div>
 												</div>
-												<div class="form-group" ng-if="Formulario.valor[servicio.id]">
-												<div class="input-group">
-													<span class="input-group-addon">Valor</span>
-													<input type="text" name="valor{{servicio.id}}" id="valor{{servicio.id}}" readonly class="form-control" 
-													ng-click="activeValor(servicio.id, servicio.idValor)">
+												<div class="col-sm-3">
+													<label >Nada Este hogar no paga</label>
+													<div class="form-group">
+														<input type="radio" name="valorotro{{servicio.id}}" value="001" 
+														ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '00')">
 													</div>
 												</div>
-											</div>
-											<div class="col-sm-3">
-												<label >Nada Este hogar no paga</label>
-												<div class="form-group">
-													<input type="radio" name="valorotro{{servicio.id}}" value="001" 
-													ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '00')">
+												<div class="col-sm-3">
+													<label >No recuerda el valor</label>
+													<div class="form-group">
+														<input type="radio" name="valorotro{{servicio.id}}" value="98" 
+														ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '98')">
+													</div>
 												</div>
-											</div>
-											<div class="col-sm-3">
-												<label >No recuerda el valor</label>
-												<div class="form-group">
-													<input type="radio" name="valorotro{{servicio.id}}" value="98" 
-													ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '98')">
+												<div class="col-sm-6">
+													<label >El valor ya fue reportado o incluido en otro servicio o valor</label>
+													<div class="form-group">
+														<input type="radio" name="valorotro{{servicio.id}}" value="99" 
+														ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '99')">
+													</div>
 												</div>
-											</div>
-											<div class="col-sm-6">
-												<label >El valor ya fue reportado o incluido en otro servicio o valor</label>
-												<div class="form-group">
-													<input type="radio" name="valorotro{{servicio.id}}" value="99" 
-													ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '99')">
-												</div>
-											</div>
 											</div>
 										</td>
 										<td>
-										<div class="form-group">
-											<select class="form-control" name="meses" id="meses" required ng-model="Formulario.servicios[servicio.idMes]">
-												<option value="" selected disabled>Seleccione</option>
-												<option ng-repeat="mes in meses" value="{{mes.id}}">{{mes.value}}</option>
-											</select>
+											<div class="form-group">
+												<select class="form-control" name="meses" id="meses" required ng-model="Formulario.servicios[servicio.idMes]">
+													<option value="" selected disabled>Seleccione</option>
+													<option ng-repeat="mes in meses" value="{{mes.id}}">{{mes.value}}</option>
+												</select>
 											</div>
 										</td>
 										<td>
@@ -155,52 +154,52 @@
 									<tr ng-repeat="servicio in servicios">
 										<th>{{servicio.servicio}}</th>
 										<td>
-										<div style="max-width: 900px;">
-											<div class="col-sm-12">
-												<div class="form-group" ng-if="!Formulario.valor[servicio.id]">
-												<div class="input-group">
-													<span class="input-group-addon">Valor</span>
-													<input type="text" name="valor{{servicio.id}}" id="valor{{servicio.id}}" required 
-													ng-model="Formulario.servicios[servicio.idValor]" class="form-control">
+											<div style="max-width: 900px;">
+												<div class="col-sm-12">
+													<div class="form-group" ng-if="!Formulario.valor[servicio.id]">
+														<div class="input-group">
+															<span class="input-group-addon">Valor</span>
+															<input type="text" name="valor{{servicio.id}}" id="valor{{servicio.id}}" required 
+															ng-model="Formulario.servicios[servicio.idValor]" class="form-control">
+														</div>
+													</div>
+													<div class="form-group" ng-if="Formulario.valor[servicio.id]">
+														<div class="input-group">
+															<span class="input-group-addon">Valor</span>
+															<input type="text" name="valor{{servicio.id}}" id="valor{{servicio.id}}" readonly class="form-control" 
+															ng-click="activeValor(servicio.id, servicio.idValor)">
+														</div>
 													</div>
 												</div>
-												<div class="form-group" ng-if="Formulario.valor[servicio.id]">
-												<div class="input-group">
-													<span class="input-group-addon">Valor</span>
-													<input type="text" name="valor{{servicio.id}}" id="valor{{servicio.id}}" readonly class="form-control" 
-													ng-click="activeValor(servicio.id, servicio.idValor)">
+												<div class="col-sm-3">
+													<label >Nada Este hogar no paga</label>
+													<div class="form-group">
+														<input type="radio" name="valorotro{{servicio.id}}" value="001" 
+														ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '00')">
 													</div>
 												</div>
-											</div>
-											<div class="col-sm-3">
-												<label >Nada Este hogar no paga</label>
-												<div class="form-group">
-													<input type="radio" name="valorotro{{servicio.id}}" value="001" 
-													ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '00')">
+												<div class="col-sm-3">
+													<label >No recuerda el valor</label>
+													<div class="form-group">
+														<input type="radio" name="valorotro{{servicio.id}}" value="98" 
+														ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '98')">
+													</div>
 												</div>
-											</div>
-											<div class="col-sm-3">
-												<label >No recuerda el valor</label>
-												<div class="form-group">
-													<input type="radio" name="valorotro{{servicio.id}}" value="98" 
-													ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '98')">
+												<div class="col-sm-6">
+													<label >El valor ya fue reportado o incluido en otro servicio o valor</label>
+													<div class="form-group">
+														<input type="radio" name="valorotro{{servicio.id}}" value="99" 
+														ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '99')">
+													</div>
 												</div>
-											</div>
-											<div class="col-sm-6">
-												<label >El valor ya fue reportado o incluido en otro servicio o valor</label>
-												<div class="form-group">
-													<input type="radio" name="valorotro{{servicio.id}}" value="99" 
-													ng-model="Formulario.valor[servicio.id]" ng-click="changeValor(servicio.idValor, '99')">
-												</div>
-											</div>
 											</div>
 										</td>
 										<td>
-										<div class="form-group">
-											<select class="form-control" name="meses" id="meses" required ng-model="Formulario.servicios[servicio.idMes]">
-												<option value="" selected disabled>Seleccione</option>
-												<option ng-repeat="mes in meses" value="{{mes.id}}">{{mes.value}}</option>
-											</select>
+											<div class="form-group">
+												<select class="form-control" name="meses" id="meses" required ng-model="Formulario.servicios[servicio.idMes]">
+													<option value="" selected disabled>Seleccione</option>
+													<option ng-repeat="mes in meses" value="{{mes.id}}">{{mes.value}}</option>
+												</select>
 											</div>
 										</td>
 										<td>
@@ -225,9 +224,37 @@
 						</div>
 					</div>
 				</form>
-				<div class="row text-center">
-					<button class="btn btn-success" ng-disabled="!FormServicios.$valid" ng-click="validateForm2(3)" id="ENV_2_2">Guardar y Continuar <span class="glyphicon glyphicon-chevron-right" aria-hidden="true" title="Continuar"></span></button>
+				<div class="row text-center" ng-if="FormServicios.$valid">
+						<button class="btn btn-success" id="ENV_2_2" data-toggle="modal" data-target="#modalPage1">Guardar y Continuar
+							<span class="glyphicon glyphicon-chevron-right" aria-hidden="true" title="Continuar"></span>
+						</button>
+					</div>
+				<div class="row text-center" ng-if="!FormServicios1.$valid">
+					<button class="btn btn-success" ng-click="validateContinue(1)" id="ENV_2_2">Guardar y Continuar
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true" title="Continuar"></span>
+					</button>
 				</div>
+
+				<!-- Modal -->
+			<div class="modal fade" id="modalPage1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+							<h4 class="modal-title" id="myModalLabel">Confirmación</h4>
+						</div>
+						<div class="modal-body">
+							¿Está seguro de querer continuar? Una vez haga clic en Continuar NO podrá cambiar la información proporcionada y NO podrá regresar a esta pantalla. Si quiere editar información de estas respuestas haga clic en Cancelar.
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+							<button type="button" class="btn btn-success" ng-click="validateForm2(3)">Continuar</button>
+						</div>
+					</div>
+				</div>
+			</div> 
+			<!-- Modal -->
+
 			</div>
 		</div>
 	</div>
--- js/modgasmenfrehogar/seccionsDEF/controller.js
+++ js/modgasmenfrehogar/seccionsDEF/controller.js
@@ -823,7 +823,7 @@ appGHogar.controller('Educacion', ['$scope', 'dataService', 'localStorageService
 
 }]);
 
-appGHogar.controller('SeccionC', ['$scope', 'dataService', 'localStorageService', '$window', function($scope, dataService, localStorageService, $window) {
+appGHogar.controller('SeccionC', ['$scope', 'dataService', 'localStorageService', '$window', '$filter', function($scope, dataService, localStorageService, $window, $filter) {
 
 	console.log('hola');
 	$scope.meses = [
@@ -836,140 +836,232 @@ appGHogar.controller('SeccionC', ['$scope', 'dataService', 'localStorageService'
 		"value": "2"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "4",
+		"value": "4"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "5",
+		"value": "5"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "6",
+		"value": "6"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "7",
+		"value": "7"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "8",
+		"value": "8"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "9",
+		"value": "9"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "10",
+		"value": "10"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "11",
+		"value": "11"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "12",
+		"value": "12"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "13",
+		"value": "13"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "14",
+		"value": "14"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "15",
+		"value": "15"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "16",
+		"value": "16"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "17",
+		"value": "17"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "18",
+		"value": "18"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "19",
+		"value": "19"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "20",
+		"value": "20"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "21",
+		"value": "21"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "22",
+		"value": "22"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "23",
+		"value": "23"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "24",
+		"value": "24"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "25",
+		"value": "25"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "26",
+		"value": "26"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "27",
+		"value": "27"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "28",
+		"value": "28"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "29",
+		"value": "29"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "30",
+		"value": "30"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "31",
+		"value": "31"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "32",
+		"value": "32"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "33",
+		"value": "33"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "34",
+		"value": "34"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "35",
+		"value": "35"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "36",
+		"value": "36"
 	},
 	{
-		"id": "3",
-		"value": "3"
+		"id": "37",
+		"value": "37"
+	},
+	{
+		"id": "38",
+		"value": "38"
+	},
+	{
+		"id": "39",
+		"value": "39"
+	},
+	{
+		"id": "40",
+		"value": "40"
+	},,
+	{
+		"id": "41",
+		"value": "41"
+	},
+	{
+		"id": "42",
+		"value": "42"
+	},
+	{
+		"id": "43",
+		"value": "43"
+	},
+	{
+		"id": "44",
+		"value": "44"
+	},
+	{
+		"id": "45",
+		"value": "45"
+	},
+	{
+		"id": "46",
+		"value": "46"
+	},
+	{
+		"id": "47",
+		"value": "47"
+	},
+	{
+		"id": "48",
+		"value": "48"
+	},
+	{
+		"id": "49",
+		"value": "49"
+	},
+	{
+		"id": "50",
+		"value": "50"
+	},
+	{
+		"id": "51",
+		"value": "51"
+	},
+	{
+		"id": "52",
+		"value": "52"
+	},
+	{
+		"id": "53",
+		"value": "53"
+	},
+	{
+		"id": "54",
+		"value": "54"
+	},
+	{
+		"id": "55",
+		"value": "55"
+	},
+	{
+		"id": "56",
+		"value": "56"
+	},
+	{
+		"id": "57",
+		"value": "57"
+	},
+	{
+		"id": "58",
+		"value": "58"
+	},
+	{
+		"id": "59",
+		"value": "59"
+	},
+	{
+		"id": "60",
+		"value": "60"
 	}
 	];
 
@@ -1042,7 +1134,9 @@ appGHogar.controller('SeccionC', ['$scope', 'dataService', 'localStorageService'
 	}
 	];
 
-	$scope.Formulario = {};
+	$scope.Formulario = {
+		"valorVariable": ""
+	};
 	$scope.validateGroup = [];
 	$scope.subtotal = 0;
 
@@ -1060,6 +1154,14 @@ appGHogar.controller('SeccionC', ['$scope', 'dataService', 'localStorageService'
 	dataService.getElements(paramsInit, function(dataResponse){
 		$scope.Formulario.rh = dataResponse;
 	});*/
+
+	$scope.validateContinue = function(params){
+		console.log('hi');
+	};
+
+	$scope.validateBtnS1 = function(){
+		
+	};
 
 
 	$scope.validateForm1 = function(params){
